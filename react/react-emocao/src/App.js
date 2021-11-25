6a4beda5bb63f1b6c7ca4772df43814f12c79179
gusta <gustavocarvalhodasilva015@gmail.com>
2021-11-24 23:50:31 -0300

    minhas emocoes react

---
+++ react/react-emocao/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function app(){
+  
+
+  return (
+    <div>
+      <h1>Minha emoções</h1>
+      <p> Medo </p>
+      <p> Satisfação </p>
+      <p> Vergonha </p>
+      <p> Tédio </p>
+      <p> Admiração </p>
+      <p> Diversão </p>
+      <p> Alegria </p>
+      <p> Compaixão </p>
+      <p> Amor </p>
+
+    </div>
+  );
+}
+
+
+export default app;
